5d10c0eaa6cc45395e884501cb298c26df50d8ef
KolodziejczykWaldemar <kolodziejczykwaldemar222@gmail.com>
2019-12-15 19:15:38 +0100

    Added two types of variables

--- examples.py
+++ examples.py
@@ -7,24 +7,56 @@ from sklearn.ensemble import RandomForestRegressor
 
 from partial_dependence_plot import plot_ice, plot_pdp
 
-dataset = pd.DataFrame(data=fetch_california_housing()['data'], columns=fetch_california_housing()['feature_names'])
-features = dataset.values[:10000, :]
-y = fetch_california_housing()['target'][:10000]
+# dataset = pd.DataFrame(data=fetch_california_housing()['data'], columns=fetch_california_housing()['feature_names'])
+# features = dataset.values[:10000, :]
+# y = fetch_california_housing()['target'][:10000]
+
+from sklearn.datasets import load_boston
+print(load_boston()['DESCR'])
+dataset = pd.DataFrame(data=load_boston()['data'], columns=load_boston()['feature_names'])
+features = dataset.values
+y = load_boston()['target']
 
 model = RandomForestRegressor(n_estimators=200, max_depth=15, min_samples_leaf=10)
 model.fit(features, y)
 
-# plot_df = pd.DataFrame()
-# for f in range(len(dataset.columns)):
-#     vals = plot_pdp(features=features, model=model, feature_number=f,
-#                                         feature_name=dataset.columns[f])
-#     plot_df[dataset.columns[f]] = vals
-#     plot_df.plot(figsize=(16, 9))
-#     plt.show()
 
-plot_df = pd.DataFrame()
 for f in range(len(dataset.columns)):
-    res_df = plot_ice(features=features, model=model, feature_number=f, feature_name=dataset.columns[f])
-    ax = res_df.plot(figsize=(16, 9), c='k', alpha=0.08, legend=False, linewidth=0.5)
-    res_df.mean(axis=1).plot(c='red', linewidth=5, legend=False, ax=ax)
+    if dataset.columns[f] == 'RAD':
+        feature_type = 'discrete'
+    else:
+        feature_type = 'continuous'
+
+    plot_df = plot_pdp(features=features, model=model, feature_number=f,
+                                        feature_name=dataset.columns[f], feature_type=feature_type)
+
+    if feature_type == 'discrete':
+        plot_df.plot(figsize=(16, 9), legend=False, marker='o', linewidth=0.2, markersize=3, color='black')
+    elif feature_type == 'continuous':
+        plot_df.plot(figsize=(16, 9), legend=False, linewidth=1, color='black')
+        ax = plt.gca()
+        bottom, top = ax.get_ylim()
+        indicator_height = 0.05 * (top - bottom)
+        ax.plot(dataset[dataset.columns[f]].values, [bottom - indicator_height]*dataset.shape[0], '|', color='k', alpha=0.08)
+    plt.show()
+
+
+for f in range(len(dataset.columns)):
+    if dataset.columns[f] == 'RAD':
+        feature_type = 'discrete'
+    else:
+        feature_type = 'continuous'
+
+    res_df = plot_ice(features=features, model=model, feature_number=f, feature_name=dataset.columns[f], feature_type=feature_type)
+    if feature_type == 'discrete':
+        ax = res_df.plot(figsize=(16, 9), legend=False, marker='o', linewidth=0.5, markersize=1, color='black', alpha=0.08)
+        res_df.mean(axis=1).plot(c='red', linewidth=1, legend=False, ax=ax, marker='o', markersize=7)
+    elif feature_type == 'continuous':
+        ax = res_df.plot(figsize=(16, 9), c='k', alpha=0.08, legend=False, linewidth=0.5)
+        res_df.mean(axis=1).plot(c='red', linewidth=5, legend=False, ax=ax)
+
+        bottom, top = ax.get_ylim()
+        indicator_height = 0.05 * (top - bottom)
+        ax.plot(dataset[dataset.columns[f]].values, [bottom - indicator_height] * dataset.shape[0], '|', color='k',
+                alpha=0.08)
     plt.show()
--- partial_dependence_plot.py
+++ partial_dependence_plot.py
@@ -2,21 +2,27 @@ import numpy as np
 import pandas as pd
 
 
-def plot_pdp(features, model, feature_number, feature_name):
-    # TODO add types of variables, continous and discrete
+def plot_pdp(features, model, feature_number, feature_name, feature_type='continuous'):
     # TODO add docstring
-    sample_indexes = np.random.randint(features.shape[0], size=1000)
-    features = features[sample_indexes, :]
+    # TODO create separate function for sampling
 
-    sample_size = 100
-    min_value = features[:, feature_number].min()
-    max_value = features[:, feature_number].max()
-    sampled_values = np.linspace(start=min_value, stop=max_value, num=sample_size)
+    if features.shape[0] > 1000:
+        sample_indexes = np.random.randint(features.shape[0], size=1000)
+        features = features[sample_indexes, :]
+
+    if feature_type == 'continuous':
+        sample_resolution = 100
+        min_value = features[:, feature_number].min()
+        max_value = features[:, feature_number].max()
+        sampled_values = np.linspace(start=min_value, stop=max_value, num=sample_resolution)
+    elif feature_type == 'discrete':
+        sampled_values = np.unique(features[:, feature_number], return_index=False)
+        sample_resolution = sampled_values.shape[0]
 
     stacked_instances = np.empty((0, features.shape[1]), float)
     other_features = np.delete(features, feature_number, axis=1)
     for i, row in enumerate(other_features):
-        copied_instances = np.repeat(row.reshape(1, -1), sample_size, axis=0)
+        copied_instances = np.repeat(row.reshape(1, -1), sample_resolution, axis=0)
         concatenated_instances = np.insert(copied_instances, feature_number, sampled_values.ravel(), axis=1)
         stacked_instances = np.append(stacked_instances, concatenated_instances, axis=0)
 
@@ -25,30 +31,36 @@ def plot_pdp(features, model, feature_number, feature_name):
                                     'output': y_pred})
     mean_outputs = feature_results.groupby([feature_name]).mean()
 
-    return mean_outputs['output'].values
+    return mean_outputs
 
 
-def plot_ice(features, model, feature_number, feature_name):
-    # TODO add types of variables, continous and discrete
+def plot_ice(features, model, feature_number, feature_name, feature_type='continuous'):
     # TODO add docstring
-    sample_indexes = np.random.randint(features.shape[0], size=1000)
-    features = features[sample_indexes, :]
+    # TODO create separate function for sampling
 
-    sample_size = 100
-    min_value = features[:, feature_number].min()
-    max_value = features[:, feature_number].max()
-    sampled_values = np.linspace(start=min_value, stop=max_value, num=sample_size)
+    if features.shape[0] > 1000:
+        sample_indexes = np.random.randint(features.shape[0], size=1000)
+        features = features[sample_indexes, :]
+
+    if feature_type == 'continuous':
+        sample_resolution = 100
+        min_value = features[:, feature_number].min()
+        max_value = features[:, feature_number].max()
+        sampled_values = np.linspace(start=min_value, stop=max_value, num=sample_resolution)
+    elif feature_type == 'discrete':
+        sampled_values = np.unique(features[:, feature_number], return_index=False)
+        sample_resolution = sampled_values.shape[0]
 
     stacked_instances = np.empty((0, features.shape[1]), float)
     other_features = np.delete(features, feature_number, axis=1)
 
     row_indicator = []
     for i, row in enumerate(other_features):
-        copied_instances = np.repeat(row.reshape(1, -1), sample_size, axis=0)
+        copied_instances = np.repeat(row.reshape(1, -1), sample_resolution, axis=0)
         concatenated_instances = np.insert(copied_instances, feature_number, sampled_values.ravel(), axis=1)
         stacked_instances = np.append(stacked_instances, concatenated_instances, axis=0)
 
-        row_indicator += ((np.ones(sample_size) * i).ravel().tolist())
+        row_indicator += ((np.ones(sample_resolution) * i).ravel().tolist())
 
     y_pred = model.predict(stacked_instances).ravel()
     feature_results = pd.DataFrame({feature_name: stacked_instances[:, feature_number],
