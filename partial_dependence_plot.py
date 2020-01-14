cb49e94f494d44924ba4d916aa6252d0a97e53f1
waldemar <waldemar.kolodziejczyk@atendesoftware.pl>
2020-01-14 19:10:06 +0100

    Add 2D PDP plots

--- partial_dependence_plot.py
+++ partial_dependence_plot.py
@@ -1,3 +1,5 @@
+import itertools
+
 import numpy as np
 import pandas as pd
 
@@ -6,7 +8,7 @@ def plot_pdp(model: object,
              X: np.ndarray,
              feature_number: int,
              feature_name: str,
-             feature_type='continuous') -> pd.DataFrame:
+             feature_type: str='continuous') -> pd.DataFrame:
     """
     Function outputs Partial Dependence Plot for given feature number. Implementation based on
     https://christophm.github.io/interpretable-ml-book/pdp.html
@@ -54,11 +56,66 @@ def plot_pdp(model: object,
     return mean_outputs
 
 
+def plot_pdp_2d(model: object,
+                X: np.ndarray,
+                feature_numbers: list,
+                feature_names: list,
+                feature_types: list='continuous') -> pd.DataFrame:
+    """
+    Function outputs Partial Dependence Plot for given feature number. Implementation based on
+    https://christophm.github.io/interpretable-ml-book/pdp.html
+
+    Args:
+        model: (object) fitted model with standard predict(X) public method
+        X: (numpy.ndarray) multidimensional array of input to the given model
+        feature_numbers: (list) list of feature indexes to compute PDP for, where feature vector will be X[:, feature_numbers]
+        feature_names: (list) list of feature names
+        feature_types: (str) list of feature types - either "discrete" or "continuous"
+    Returns:
+        (pandas.DataFrame) dataframe with indexes as first feature, columns as second feature and mean PDP values.
+    """
+
+    if X.shape[0] > 1000:
+        sample_indexes = np.random.randint(X.shape[0], size=1000)
+        X = X[sample_indexes, :]
+
+    sampled_space = __sample_2d_space(X[:, feature_numbers],
+                                      feature_types,
+                                      sample_resolution=100)
+    sample_resolution = sampled_space.shape[0]
+
+    stacked_instances = np.empty((0, X.shape[1]), float)
+
+    for i, row in enumerate(X):
+        copied_instances = np.repeat(row.reshape(1, -1),
+                                     sample_resolution,
+                                     axis=0)
+        copied_instances[:, feature_numbers[0]] = sampled_space[:, 0]
+        copied_instances[:, feature_numbers[1]] = sampled_space[:, 1]
+
+        stacked_instances = np.append(stacked_instances,
+                                      copied_instances,
+                                      axis=0)
+
+    y_pred = model.predict(stacked_instances).ravel()
+    feature_results = pd.DataFrame({feature_names[0]: stacked_instances[:, feature_numbers[0]],
+                                    feature_names[1]: stacked_instances[:, feature_numbers[1]],
+                                    'output': y_pred})
+
+    pdp_map = pd.pivot_table(feature_results,
+                             values='output',
+                             index=[feature_names[0]],
+                             columns=[feature_names[1]],
+                             aggfunc=np.mean)
+
+    return pdp_map
+
+
 def plot_ice(model: object,
              X: np.ndarray,
              feature_number: int,
              feature_name: str,
-             feature_type='continuous') -> pd.DataFrame:
+             feature_type: str='continuous') -> pd.DataFrame:
     """
     Function outputs Individual Conditional Expectation for given feature number. Implementation based on
     https://christophm.github.io/interpretable-ml-book/ice.html
@@ -121,7 +178,7 @@ def plot_ice(model: object,
 
 def __sample_space(x: np.ndarray,
                    feature_type: str,
-                   sample_resolution=100) -> np.ndarray:
+                   sample_resolution: int=100) -> np.ndarray:
     """
     Function samples space according to feature_type.
 
@@ -140,4 +197,28 @@ def __sample_space(x: np.ndarray,
         sampled_values = np.unique(x, return_index=False)
 
     return sampled_values
+
+
+def __sample_2d_space(features: np.ndarray,
+                      feature_types: list,
+                      sample_resolution: int=100) -> np.ndarray:
+    """
+    Function samples 2D space according to feature_types.
+
+    Args:
+        features: (numpy.ndarray) 2-D array of two feature values features.shape[1] = 2
+        feature_types: (list) list of feature types - either "discrete" or "continuous"
+        sample_resolution: (int) size of sampled space
+    Returns:
+        (numpy.ndarray) vector of sample space with shape of (:, 2).
+    """
+    x1 = features[:, 0]
+    x2 = features[:, 1]
+
+    sampled_x1 = __sample_space(x1, feature_types[0], sample_resolution=sample_resolution)
+    sampled_x2 = __sample_space(x2, feature_types[1], sample_resolution=sample_resolution)
+
+    combinations_list = list(itertools.product(sampled_x1, sampled_x2))
+    sample_space = np.asarray(combinations_list)
+    return sample_space
 
